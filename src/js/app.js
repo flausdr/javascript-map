f096e77b2ed5cd27fea3448c4776a65ce8acb8f4
Flaus-react-pc <mrychkov1302@gmail.com>
2022-07-28 11:14:30 +0300

    initilization markers on map

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,58 @@
+const fetchFlats = async () => {
+    return await fetch('../flats.txt')
+    .then(response => {
+        if (response.status === 200 && response.ok) {
+            return response.json()
+        } else {
+            return new Error(response.statusText)
+        }
+    })
+}
+
+const markersForInitMap = () => {
+    const newArray = [];
+    const fetch = fetchFlats();
+    fetch.then(flats => {
+        flats.map(flat => {
+            newArray.push(flat);
+        })
+    })
+    return newArray;
+}
+
+const flatsArrayObj = {
+    flats: markersForInitMap(),
+    newFlats: [],
+}
+
+const initMarker = (elem, map) => {
+    const position = {
+        lat: Number(elem.lat),
+        lng: Number(elem.lng),
+    }
+
+    const marker = new google.maps.Marker({
+        position,
+        map
+    });
+
+    return marker;
+}
+
+const finalStep = (arr, map) => {
+    return arr.map(elem => {
+        return initMarker(elem, map);
+    })
+}
+
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: { lat: 49.594085, lng: 34.543770 },
     });
+
+    setTimeout(() => {
+        const markers = finalStep(flatsArrayObj.flats, map);
+        new markerClusterer.MarkerClusterer({ map, markers });
+    }, 500)
 }
