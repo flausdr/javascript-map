90b9a5e9fb26459043198e99a0d77c1430975184
Flaus-react-pc <mrychkov1302@gmail.com>
2022-08-18 21:22:43 +0300

    create filters

--- src/js/app.js
+++ src/js/app.js
@@ -2,24 +2,50 @@ import '../style/style.scss'
 
 import InitMap from './map/init-map'
 import InitFlats from './map/fetch'
-import Cards from './dynamic-info/loadCards'
+import Cards from './dynamic-info/add-info'
 import Content from './map/popupCard'
 import RightBlock from './dynamic-info/right-block'
+import Filters from './dynamic-info/filter'
 
 document.addEventListener('DOMContentLoaded', async () => {
     const map = new InitMap()
     const flats = new InitFlats()
-    const cards = new Cards()
     const rightBlock = new RightBlock()
-
+    
     await flats.fetchFlats()
     map.initMarkers(flats.flats)
-
+    
     rightBlock.initSelectsOptions(flats.flats)
     rightBlock.windowScroll(flats.flats)
-
+    
     document.querySelector('.wrapper').addEventListener('click', (e) => {
         const content = new Content()
         content.initPopup(e)
+    })
+    
+    const filter = new Filters()
+    document.querySelector('.container').addEventListener('click', e => {
+        const minRange = document.querySelector('.range-min'),
+            maxRange = document.querySelector('.range-max'),
+            zeroStreet = document.querySelector('.streetlist__search'),
+            zeroRooms = document.querySelector('.open__zero')
+        if (e.target.classList.contains('streetlist__li')) {
+            const street = e.target.innerText
+            map.updateLayers()
+            map.initMarkers(filter.filterStreet(flats.flats, street))
+            zeroRooms.innerHTML = zeroRooms.dataset.first
+        }
+        if (e.target.classList.contains('rooms-filter-options__value')) {
+            const rooms = e.target.innerText
+            map.updateLayers()
+            map.initMarkers(filter.filterRooms(flats.flats, rooms))
+            zeroStreet.value = ''
+        }
+        if (e.target.classList.contains('price-range__input') || e.target.classList.contains('price-values__input')) {
+            map.updateLayers()
+            map.initMarkers(filter.filterPrice(flats.flats, minRange.value, maxRange.value))
+            zeroStreet.value = ''
+            zeroRooms.innerHTML = zeroRooms.dataset.first
+        }
     })
 })
--- src/js/map/init-map.js
+++ src/js/map/init-map.js
@@ -1,28 +1,40 @@
 import L from 'leaflet'
 
 export default class InitMap {
-
     constructor() {
-        this.map = L.map('map').setView([49.587085, 34.543770], 13)
-        this.tileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map)
+        this.map = L.map('map').setView([49.587085, 34.54377], 13)
+        this.tileLayer = L.tileLayer(
+            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        ).addTo(this.map)
         this.layer = L.layerGroup().addTo(this.map)
     }
 
     initMarkers(arrayFlats) {
-        arrayFlats.forEach(flat => {
+        arrayFlats.forEach((flat) => {
             const info = `<span class="hide" data-street="${flat.street}" data-price="${flat.price}"></span>`
 
             const iconDiv = L.divIcon({
                 className: 'my-div-icon',
-                html: info
+                html: info,
             })
-
 
             L.marker([flat.lat, flat.lng], {
                 icon: iconDiv,
                 alt: flat.street,
-                opacity: 0.9
+                opacity: 0.9,
             }).addTo(this.layer)
+        })
+    }
+
+    updateLayers() {
+        let layerArr = this.layer.getLayers()
+
+        this.layer.eachLayer((layer) => {
+            let layerIndex = layerArr.indexOf(layer)
+
+            if (layerIndex !== 0 || layerIndex !== 1) {
+                this.layer.removeLayer(layer)
+            }
         })
     }
 }
